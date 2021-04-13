b56cdb1a743236399a178c79c45eda5de41d1930
Lucas <onitrog0d@gmail.com>
2021-04-13 15:15:01 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# nitro-http-client
+[![npm release](https://img.shields.io/npm/dt/nitro-http-client?style=for-the-badge)](https://www.npmjs.com/package/nitro-http-client)
+[![Support me on Patreon](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fwww.patreon.com%2Fapi%2Fcampaigns%2F1177520&query=data.attributes.patron_count&suffix=%20Patrons&color=FF5441&label=Patreon&logo=Patreon&logoColor=FF5441&style=for-the-badge)](https://patreon.com/nitrog0d)
+
+A HTTP client module I made because I can't deal with other modules  
+It does not support/handle compression, I'll eventually implement it according to my needs
+
+## Installation
+
+* NPM: `npm install nitro-http-client`  
+* Yarn: `yarn add nitro-http-client`
+
+## Usage
+
+### JavaScript:
+**Create the client**
+```js
+const nitroHttpClient = require('nitro-http-client');
+const httpClient = new nitroHttpClient.NitroHttpClient();
+
+// You can set optional settings in the constructor
+// Default values are true
+/*const httpClient = new nitroHttpClient.NitroHttpClient({
+  responseBodyAsString: false,
+  setContentLengthAutomatically: true
+});*/
+```
+
+### TypeScript
+**Create the client**
+```ts
+import { NitroHttpClient } from 'nitro-http-client';
+const httpClient = new NitroHttpClient();
+
+// You can set optional settings in the constructor
+// Default values are true
+/*const httpClient = new NitroHttpClient({
+  responseBodyAsString: false,
+  setContentLengthAutomatically: true
+});*/
+```
+
+### Examples
+**Example with default values, no body, GET method, no extra headers**
+```js
+const response = await httpClient.request('https://example.com');
+console.log(response.statusCode);
+```
+
+**.then example since it returns a Promise**
+```js
+httpClient.request('https://example.com').then(response => {
+  console.log(response.body);
+});
+
+```
+**Example with custom values**
+```js
+const response = await httpClient.request('https://example.com', {
+  method: 'POST',
+  headers: {
+    Authentication: '123',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ test: 123 })
+});
+console.log(response.headers);
+```
---
+++ src/index.ts
@@ -0,0 +1,101 @@
+import http from 'http';
+import https from 'https';
+
+export interface HttpResponse {
+  body?: string | Buffer;
+  headers?: http.OutgoingHttpHeaders;
+  statusCode?: number;
+  statusMessage?: string;
+}
+
+export interface HttpRequestOptions {
+  body?: string | Buffer;
+}
+
+export interface ClientSettings {
+  setContentLengthAutomatically?: boolean;
+  responseBodyAsString?: boolean;
+}
+
+export function headersFromRawHeaders(rawHeaders: string[]) {
+  if (rawHeaders.length === 0) return {};
+  const convertedHeaders: http.OutgoingHttpHeaders = {};
+  const length = rawHeaders.length / 2;
+  for (let i = 0; i < length; i++) convertedHeaders[rawHeaders.shift()!] = rawHeaders.shift();
+  return convertedHeaders;
+}
+
+export function mapToDict(map: Map<string, string>) {
+  const dict: http.OutgoingHttpHeaders = {};
+  map.forEach((value, key) => dict![key] = value);
+  return dict;
+}
+
+export function dictToMap(dict: http.OutgoingHttpHeaders) {
+  const map = new Map<string, string>();
+  Object.entries(dict).map(value => map.set(value[0], value[1] as string));
+  return map;
+}
+
+export class NitroHttpClient {
+
+  public clientSettings: ClientSettings = {
+    responseBodyAsString: true,
+    setContentLengthAutomatically: true
+  };
+
+  constructor(clientSettings?: ClientSettings) {
+    if (clientSettings) {
+      if (clientSettings.setContentLengthAutomatically !== undefined) this.clientSettings.setContentLengthAutomatically = clientSettings.setContentLengthAutomatically;
+      if (clientSettings.responseBodyAsString !== undefined) this.clientSettings.responseBodyAsString = clientSettings.responseBodyAsString;
+    }
+  }
+
+  public async request(url: string, options?: HttpRequestOptions & https.RequestOptions): Promise<HttpResponse> {
+    return await new Promise((resolve, reject) => {
+      try {
+        const parsedUrl = new URL(url);
+        const isHttps = parsedUrl.protocol === 'https:';
+        if (!options) options = {};
+
+        const newHeaders: http.OutgoingHttpHeaders = {
+          Host: parsedUrl.host
+        }
+        if (options.headers) Object.entries(options.headers).map(value => newHeaders[value[0]] = value[1]);
+        options.headers = newHeaders;
+
+        console.log(options.headers);
+
+        options.host = parsedUrl.host;
+        options.port = parsedUrl.port.length !== 0 ? parsedUrl.port : isHttps ? 443 : 80;
+        options.path = parsedUrl.pathname + parsedUrl.search;
+
+        if (this.clientSettings.setContentLengthAutomatically && !options.headers['Content-Length'] && options.body) options.headers['Content-Length'] = options.body.length;
+
+        const callback = (res: http.IncomingMessage) => {
+          const chunks: Buffer[] = [];
+
+          res.on('data', (chunk: Buffer) => {
+            chunks.push(chunk);
+          });
+
+          res.on('end', () => {
+            const finalBuffer = Buffer.concat(chunks);
+            resolve({ body: finalBuffer.length !== 0 ? (this.clientSettings.responseBodyAsString ? finalBuffer.toString('utf8') : finalBuffer) : undefined, headers: res.rawHeaders.length !== 0 ? headersFromRawHeaders(res.rawHeaders) : undefined, statusCode: res.statusCode, statusMessage: res.statusMessage });
+          });
+
+          res.on('error', (error) => {
+            reject(error);
+          });
+        }
+
+        const request = isHttps ? https.request(options, callback) : http.request(options, callback);
+
+        if (options.body) request.write(options.body);
+        request.end();
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+}
